74ed78ba748923d1f4cb825359c05447e8cc4c46
oPensyLar <vgomez@protonmail.com>
2021-01-22 17:21:46 -0400

    workeando

--- report.py
+++ report.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python
 
-import os
 import datetime
-import sys
-import platform
-import socket
 import json
-
+import subprocess
+import dns.resolver
+from ipaddress import ip_address, ip_network
 from jinja2 import Environment, FileSystemLoader
 
 env = Environment(
@@ -16,29 +14,57 @@ env = Environment(
 template = env.get_template('template.html.j2')
 
 
-def pinghost(hostname):
-    # Check os type to determine which ping command to use
-    os_type = platform.platform()
-    if "Windows" in os_type:
-        response = os.system("ping -n 1 {0}".format(hostname))
-    else:
-        response = os.system("ping -c 1 {0}".format(hostname))
-    return True if response == 0 else False
+def is_ip_range(c_addr):
+    var = c_addr.find("/")
+    return var
 
 
-def checksock(hostname, port):
-    if not isinstance(port, int):
-        try:
-            port = int(port)
-        except ValueError:
-            print('Port number is not numeric!')
-            sys.exit()
+def dns_resolver(ip_addr):
+    my_resolver = dns.resolver.Resolver()
+    nam = None
+
     try:
-        socket.create_connection((hostname, port), 2)
-        return True
-    except socket.error:
-        print('%s failed on port: %s' % (hostname, str(port)))
-        return False
+        answer = my_resolver.resolve_address(ip_addr)
+
+    except dns.exception.Timeout:
+        return nam
+
+    for g in answer.response.answer:
+        for a in g.items:
+            nam = a.target
+            return str(nam)
+
+
+def os_detect(ttl_val):
+    if ttl_val is 64:
+        return "Linux"
+
+    if ttl_val == 128:
+        return "Windows"
+
+
+def parse_output_ping(output_string):
+    output_string = output_string.decode("utf-8")
+
+    ttl_numb = "0"
+
+    if output_string.find("TTL") > 0x0:
+        # Begin TTL extraction
+        pos_init = output_string.find("TTL")
+        pos_init += 4
+        output_string = output_string[pos_init:]
+        output_string_split = output_string.split("\r")
+        ttl_numb = output_string_split[0]
+        # End TTL extraction
+
+    ret = {"ttl": int(ttl_numb)}
+    return ret
+
+
+def run_ping2(hst):
+    array_program = ["ping", "-n", "1", hst]
+    h = subprocess.Popen(array_program, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    return h.communicate()
 
 
 def parsehost(hostfile):
@@ -46,17 +72,14 @@ def parsehost(hostfile):
     with open(hostfile, "r") as f:
         data = json.load(f)
         for item in data:
-                if not isinstance(item["port"], int):
-                    print("%s Port: %s is not a number!" % (item["url"], item["port"]))
-                    sys.exit()
-
                 if item["alias"]:
-                    servers.append({"hostname": item["url"], "port": item["port"], "name": item["alias"]})
+                    servers.append({"hostname": item["url"], "name": item["alias"]})
                 else:
-                    servers.append({"hostname": item["url"], "port": item["port"], "name": item["url"]})
+                    servers.append({"hostname": item["url"], "name": item["url"]})
     return servers
 
 
+# host_dict Ya esta creado, mete las variables en el HTML
 def createhtml(output_file_name, host_dict):
     refresh_rate = "60"
     today = (datetime.datetime.now())
@@ -80,21 +103,48 @@ def createhtml(output_file_name, host_dict):
                     server_total=server_total,
                     host_dict=host_dict).dump('index.html')
 
-
+# Modifica las variables del dict
 def main():
-    names_list = "hostnames.json"
-    # put the path that you would like the report to be written to
+    f_nam = "srv.txt"
     output_file_name = "index.html"
-    hosts = parsehost(names_list)
+    hosts = []
+
+    with open(f_nam, "r") as f:
+        for c_addr in f:
+            c_addr = c_addr.replace("\n", "")
+            c_addr = c_addr.replace("\r", "")
+
+            if is_ip_range(c_addr) > 0x0:
+                net1 = ip_network(c_addr, strict=False)
+
+                for addr in net1:
+                    addr = str(addr)
+                    hosts.append({"hostname": addr})
+
+            else:
+                hosts.append({"hostname": c_addr})
+
+    # Setea las variables del dict
     for h in hosts:
-        if h.get("port"):
-            alive = checksock(h.get("hostname"), h.get("port"))
+        print("[+] Checking " + h.get("hostname"))
+
+        std_out, std_error = run_ping2(h.get("hostname"))
+        ping_vals = parse_output_ping(std_out)
+
+        h.update(ip_addr=h.get("hostname"))
+
+        if ping_vals["ttl"] is not 0x0:
+            h.update(status="up")       # Vive
+            dns_nam = dns_resolver(h.get("hostname"))
+            os_nam = os_detect(ping_vals["ttl"])
+            h.update(dns_name=dns_nam)
+            h.update(os=os_nam)
+
         else:
-            alive = pinghost(h.get("hostname"))
-        if alive:
-            h.update(status="up")
-        else:
-            h.update(status="down")
+            h.update(status="down")     # Dead
+            h.update(dns_name="None")
+            h.update(os="Unknow")
+
     createhtml(output_file_name, hosts)
 
 
